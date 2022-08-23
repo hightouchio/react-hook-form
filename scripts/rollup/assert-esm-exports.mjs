eaba44ed6c812417bec5e3322e11fb14b2851ab6
Ernest Cheng <ernestcheng2@gmail.com>
2022-08-23 22:49:28 +0800

    remove postbuild script

--- scripts/rollup/assert-esm-exports.mjs
+++ scripts/rollup/assert-esm-exports.mjs
@@ -4,23 +4,20 @@
  *
  * @see https://nodejs.org/docs/latest/api/packages.html#packages_determining_module_system
  */
-import * as exported from 'react-hook-form';
+import * as exported from '@hightouchio/react-hook-form';
 import assert from 'assert';
 import fs from 'fs';
 import path from 'path';
 import url from 'url';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 /**
  * A shell one-liner to update this array when neccessary (run from root of repo):
  *  node -e "import('react-hook-form').then((mod) => console.log(JSON.stringify(Object.keys(mod), null, 2)))" > scripts/rollup/all-exports.json
  */
 const expected = JSON.parse(
-  fs.readFileSync(
-    path.resolve(__dirname, './all-exports.json'),
-    'utf-8',
-  ),
+  fs.readFileSync(path.resolve(__dirname, './all-exports.json'), 'utf-8'),
 );
 
 assert.deepStrictEqual(Object.keys(exported), expected);
